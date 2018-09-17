1b6501df67dd0b0e1fe80300f8862e726a579143
Learner1729 <ashishpatel0894@gmail.com>
2018-09-17 15:38:21 -0400

    [Task 0]: Initiating the development...
    1. Forked cpp-boilerplate
    
    2. Created a local copy of forked repository
    
    3. Added a new branch Week_3
    
    4. Added Written Response file as a link in readme.md
    
    5. Modified the repository according to need and added the programming question

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
-#include <lib.hpp>
+
+using namespace std;
+
+void printPosition() {
+	int i,j;
+	char text[1024]="1234567890";
+	int text_length = 10;
+	char array_to_search1[4] = "23";
+	int array_to_search1_length = 2;
+	int position1 = -1;
+	for (i=0;i<text_length-array_to_search1_length+1;i++) {
+		bool found = true;
+		for (j=0;j<array_to_search1_length;j++) {
+			if (text[i+j]!=array_to_search1[j])
+				found = false;
+		}
+		if (found) {
+			position1 = i;
+			break;
+		}
+	}
+cout << "Position :" << position1 << endl;
+}
 
 int main()
 {
-    dummy();
+    printPosition();
     return 0;
 }
